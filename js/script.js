f279907c48e4d19ffbe6769f1d528af553b8990e
Eric Fu <eric.fu@mercury.eco>
2023-10-07 16:24:33 -0600

    add icons

--- js/script.js
+++ js/script.js
@@ -2,4 +2,7 @@ document.addEventListener('DOMContentLoaded', function () {
   var elems = document.querySelectorAll('.parallax');
   var options = {};
   var instances = M.Parallax.init(elems, options);
+
+  var elems = document.querySelectorAll('.collapsible');
+  var instances = M.Collapsible.init(elems, options);
 });
