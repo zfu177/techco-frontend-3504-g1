eb4d920a2f2a5703c3673c291e2870662dffa8b7
Eric Fu <eric.fu@mercury.eco>
2023-10-10 18:10:35 -0600

    update nav bar style

--- js/script.js
+++ js/script.js
@@ -5,4 +5,8 @@ document.addEventListener('DOMContentLoaded', function () {
 
   var elems = document.querySelectorAll('.collapsible');
   var instances = M.Collapsible.init(elems, options);
+
+  // Initiate sidenav
+  var elems = document.querySelectorAll('.sidenav');
+  M.Sidenav.init(elems, options);
 });
